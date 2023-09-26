ebdfd0a06cfa35800afa107b6166dc8296ea076c
Wanderson Castro <wandersonce1@hotmail.com>
2023-09-26 12:03:39 -0700

    Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
+      {/* Header */}
+      <Header />
+      {/* Board */}
+
       <h1>Wanllo</h1>
     </main>
   )
