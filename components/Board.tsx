e9396f574bfcc23a2a3ee9583026e30b1f5f0867
Wanderson Castro <wandersonce1@hotmail.com>
2023-09-27 14:18:33 -0700

    Adding board/Column dragDrop

--- components/Board.tsx
+++ components/Board.tsx
@@ -2,7 +2,8 @@
 
 import { useBoardStore } from '@/store/BoardStore';
 import{ useEffect} from 'react';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
+import Column from './Column';
 
 export default function Board() {
   const [board, getBoard] = useBoardStore((state) => [state.board, state.getBoard]);
@@ -10,19 +11,31 @@ export default function Board() {
   useEffect(() => {
     getBoard()
   }, [getBoard])
+
+  const handleOnDragEnd =(result: DropResult) => {};
   
-  console.log(board)
 
   return (
-    <h1>here</h1>
-    // <DragDropContext>
-    //   <Droppable droppableId='board' direction='horizontal' type='column'>
-    //     {(provided) => (
-    //       <div>
-    //         {/* Redenring all columns */}
-    //       </div>
-    //     )}
-    //   </Droppable>
-    // </DragDropContext>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+          <div
+          className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto'
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {/* Redenring all columns */}
+            {Array.from(board.columns.entries()).map(([id,column] ,index) => (
+              <Column 
+                key={id}
+                id={id}
+                todos={column.todos}
+                index={index}
+              />
+            ))}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   )
 }
---
+++ components/Column.tsx
@@ -0,0 +1,36 @@
+import { Draggable, Droppable } from "react-beautiful-dnd";
+
+type Props = {
+  id: TypedColumn,
+  todos: Todo[],
+  index: number
+}
+
+export default function Column({id, todos, index} : Props) {
+  return (
+    <Draggable draggableId={id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          {/* Render internal Droppable section */}
+          <Droppable droppableId={index.toString()} type="card">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={`pb-2 p-2 rounded-2xl shadow-sm ${
+                  snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
+                }`}
+              >
+                <h2>{id}</h2>
+              </div>
+            )}
+          </Droppable>
+        </div>
+      )}
+    </Draggable>
+  )
+}
