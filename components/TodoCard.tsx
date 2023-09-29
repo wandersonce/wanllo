f671a79e0e3771578cc734210d6dbebbba1fef5e
Wanderson Castro <wandersonce1@hotmail.com>
2023-09-29 12:45:38 -0700

    Adding container and styles for the card

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { XCircleIcon } from "@heroicons/react/24/solid";
 import { DraggableProvidedDragHandleProps, DraggableProvidedDraggableProps } from "react-beautiful-dnd";
 
 type Props = {
@@ -11,8 +14,22 @@ type Props = {
 
 export default function TodoCard({todo, index, id, innerRef, dragHandleProps, draggableProps} : Props) {
   return (
-    <div>
+    <div
+    className="bg-white rounded-md space-y-2 drop-shadow-md"
+      {...dragHandleProps}
+      {...draggableProps}
+      ref={innerRef}
+    >
+      <div className="flex justify-between items-center p-5">
+        <p>{todo.title}</p>
+        <button className="text-red-500 hover:text-red-600">
+          <XCircleIcon  
+            className="ml-5 h-8 w-8"
+          />
+        </button>
+      </div>
       
+      {/* Add Image if has one */}
     </div>
   )
 }
