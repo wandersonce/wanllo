e57eda4e4a825880e876a24f6ad27251a3d4891c
Wanderson Castro <wandersonce1@hotmail.com>
2023-09-29 13:03:32 -0700

    Adding setboardState

--- components/Board.tsx
+++ components/Board.tsx
@@ -6,18 +6,32 @@ import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
 import Column from './Column';
 
 export default function Board() {
-  const [board, getBoard] = useBoardStore((state) => [state.board, state.getBoard]);
+  const [board, getBoard, setBoardState] = useBoardStore((state) => [state.board, state.getBoard, state.setBoardState]);
 
   useEffect(() => {
     getBoard()
   }, [getBoard])
 
-  const handleOnDragEnd =(result: DropResult) => {};
+  const handleOnDragEnd =(result: DropResult) => {
+    const {destination, source, type} = result;
+
+    //Control the position of each board/card and handle it to keep the position after move
+    if(!destination) return;
+
+      //*Handle column drag
+      if(type==="column"){
+        const entries = Array.from(board.columns.entries());
+        const [removed] = entries.splice(source.index, 1);
+        entries.splice(destination.index, 0 , removed);
+
+        const rearrangedColumns = new Map(entries);
+      }
+  };
   
 
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
-      <Droppable droppableId="board" direction="horizontal" type="column">
+      <Droppable droppableId="boardDrop" direction="horizontal" type="column">
         {(provided) => (
           <div
           className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto'
