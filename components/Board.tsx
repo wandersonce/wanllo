f8b1166bb384d2dd036ad3e06f7de92d98bd744c
Wanderson Castro <wandersonce1@hotmail.com>
2023-09-29 13:53:08 -0700

    Drag between cols

--- components/Board.tsx
+++ components/Board.tsx
@@ -25,13 +25,74 @@ export default function Board() {
         entries.splice(destination.index, 0 , removed);
 
         const rearrangedColumns = new Map(entries);
+        setBoardState(({...board, columns: rearrangedColumns}));
       }
+
+      //*Handle card drag - This indexes are stored as numbers 0,1,2 etc. Instead of id's with DND library
+        const columns = Array.from(board.columns);
+        const startColIndex = columns[Number(source.droppableId)]
+        const finishColIndex = columns[Number(destination.droppableId)]
+
+        const startCol: Column ={
+          id: finishColIndex[0],
+          todos: startColIndex[1].todos
+        }
+
+        const finishCol: Column = {
+          id: finishColIndex[0],
+          todos: finishColIndex[1].todos
+        }
+
+        if(!startCol || !finishCol) return;
+        
+        if(source.index === destination.index && startCol === finishCol) return;
+
+        const newTodos = startCol.todos;
+        const [todoMoved] = newTodos.splice(source.index, 1)
+
+        // *Same Column task drag
+        if(startCol.id === finishCol.id){
+          newTodos.splice(destination.index, 0 , todoMoved)
+
+          const newCol = {
+            id: startCol.id,
+            todos: newTodos
+          };
+          
+          const newColumns= new Map(board.columns);
+          newColumns.set(startCol.id, newCol);
+
+          setBoardState({...board, columns:newColumns});
+
+        }else{ // *Different column task drag
+          const finishTodos = Array.from(finishCol.todos);
+          finishTodos.splice(destination.index, 0, todoMoved);
+
+          const newColumns = new Map(board.columns);
+          const newCol = {
+            id: startCol.id,
+            todos: newTodos
+          };
+
+          newColumns.set(startCol.id, newCol);
+          newColumns.set(finishCol.id, {
+            id: finishCol.id,
+            todos: finishTodos
+          });
+
+
+          //Update in DB
+
+
+          setBoardState({...board, columns:newColumns});
+
+        }
   };
   
 
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
-      <Droppable droppableId="boardDrop" direction="horizontal" type="column">
+      <Droppable droppableId="board" direction="horizontal" type="column">
         {(provided) => (
           <div
           className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto'
@@ -54,4 +115,4 @@ export default function Board() {
   )
 }
 
-//3:18
+//3:38
