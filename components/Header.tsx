2685ea4ae3013b45a08ec0f3972746e0e4180603
Wanderson Castro <wandersonce1@hotmail.com>
2023-10-03 10:28:23 -0700

    Adding state to the input

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,14 @@
 import Image from "next/image";
 import {MagnifyingGlassIcon, UserCircleIcon} from '@heroicons/react/24/solid';
 import Avatar from "react-avatar";
+import { useBoardStore } from "@/store/BoardStore";
 
 export default function Header() {
+  const [searchString, setSearchString] = useBoardStore((state) => [
+    state.searchString,
+    state.setSearchString,
+  ])
+
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
@@ -29,7 +35,13 @@ export default function Header() {
           {/* Search Box */}
           <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-400"/>
-            <input type="text" placeholder="Search" className="flex-1 outline-none p-2"/>
+            <input 
+              type="text" 
+              placeholder="Search" 
+              className="flex-1 outline-none p-2"
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
+              />
             <button hidden type="submit">Search</button>
           </form>
 
