96a02f5b415650e0360b5857d8b9e76d44ea1801
Wanderson Castro <wandersonce1@hotmail.com>
2023-10-06 12:02:15 -0700

    Adding Submit button

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -81,7 +81,7 @@ function Modal() {
 
 
           {/* FILE UPLOAD */}
-          <div>
+          <div className='mt-2'>
 
             <button 
               type='button'
@@ -115,6 +115,18 @@ function Modal() {
                 setImage(e.target.files![0]);
               }}
             />
+          </div>
+
+          <div className='mt-2'>
+            <button
+              type="submit"
+              disabled={!newTaskInput}
+              className='inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900
+              hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:text-gray-300 
+              disabled:cursor-not-allowed'
+            >
+              Add Task
+            </button>
           </div>
 
           </Dialog.Panel>
